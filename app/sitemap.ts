9fca1158171e903ee8ef1cf58f8071a211c77b60
Anubhav Gupta <anubhavgup21@gmail.com>
2026-06-22 12:06:55 +0530

    Enhance sitemap with change frequency and priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,28 @@
-import {MetadataRoute} from 'next'
-export default function sitemap() : MetadataRoute.Sitemap{
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url : 'https://www.patternflowdsa.in',
-      lastModified : new Date(),
+      url: 'https://www.patternflowdsa.in',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
     {
-      url : 'https://www.patternflowdsa.in/patterns',
-      lastModified : new Date(),
+      url: 'https://www.patternflowdsa.in/patterns',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
     },
     {
-      url : 'https://www.patternflowdsa.in/solve',
-      lastModified : new Date(),
+      url: 'https://www.patternflowdsa.in/solve',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.patternflowdsa.in/interview',
+      lastModified: new Date(),
     },
   ]
 }
